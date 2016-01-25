75b4cec5c4e966d54cd6a1b9d14cc87e9e2be8ee
Joe Talbot <z069057@googleurtwitter.com>
2016-01-25 13:37:20 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'garage-pc'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures garage-pc'
+long_description 'Installs/Configures garage-pc'
+version '0.1.0'
